23316e89de94bd852f7f69499a2194284573759e
shahiran <shahiran@vialing.com>
2026-03-15 13:31:01 +0800

    patch: fix error with test case

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,8 +3,6 @@
 use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
-uses(TestCase::class);
-
 test('login page does not offer registration when registration is disabled', function () {
     /** @var TestCase $this */
     $response = $this->get(route('login'));
